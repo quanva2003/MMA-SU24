ac8ef006e81f137bf19ea33a0e3243dafc313662
letrongdao <daoltse173161@fpt.edu.vn>
2024-07-04 20:13:28 +0700

    merge conflict

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import { useRoute } from "@react-navigation/native";
 
 const ProductScreen = () => {
+  const { params } = useRoute();
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        paddingBottom: 20,
+      }}
+      className="flex-1 bg-neutral-900"
+    >
+      <View className="w-full">
+        <SafeAreaView className="absolute z-20 w-full flex justify-between items-center px-4">
+          <TouchableOpacity>
+            <Text>&gt;</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+        <Text className="text-xl">ProductScreen</Text>
+      </View>
+    </ScrollView>
   );
 };
 
